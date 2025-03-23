05aeb88a39865f40f0a5d4c359ef10fb43123045
sa-ro <sarosaravana46452@gmail.com>
2025-03-23 17:37:58 +0530

    fix: sample favicon added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,29 @@
 import HomeComponent from "@/app/components/Home";
 import SideBar from "@/app/components/SideBar";
 import Header from "./components/Header";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Restaurant-Dashboard",
+    description: "restaurant-dashboard application",
+    openGraph: {
+      title: "Restaurant-Dashboard",
+      description: "restaurant-dashboard application",
+      images: [
+        {
+          url: "../public/Home/pizza.jpg",
+          width: 800,
+          height: 600,
+          alt: "Restaurant Dashboard",
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Restaurant-Dashboard",
+      description: "restaurant-dashboard application",
+    },
+  };
 
 const Page: React.FC = () => {
   return (
